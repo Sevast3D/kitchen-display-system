73242e5764b45ae389464a2a234a2e5fb2575c6b
Sevast3D <tutunarusergiu@gmail.com>
2023-04-22 21:05:01 +0300

    added desk variations & different buttons

--- src/components/UI/Desk/Desk.js
+++ src/components/UI/Desk/Desk.js
@@ -1,34 +1,58 @@
 import "./Desk.css";
-const Desk = () => {
+
+import Dolar from "./Assets/dollarsign.svg";
+import Eat from "./Assets/eat.svg";
+import Clean from "./Assets/clean.svg";
+
+
+function getDeskStatus(value) {
+  if (value === 1) {
+    return "orange";
+  } else if (value === 2) {
+    return "red";
+  } else if (value === 3) {
+    return "green";
+  }
+}
+
+
+const Desk = (props) => {
+  let currentTime = new Date();
   return (
-    <div className="desk" id="desk">
+    <div className={`desk ${getDeskStatus(props.value[1])}`} id="desk">
       <div className="button" id="button_container">
         <div className="status-button" id="buttons_container2">
-          <button className="add">
-            <img className="vector-icon" alt="" src="/eat.svg" />
-            <div className="add1">Add</div>
+          <button className="add-btn">
+            <img className="vector-icon" alt="" src=
+              {props.value[1] === 3 ? Clean : Eat} />
+            <div className="font-size-16">
+              {props.value[1] === 3 ? "Clean" : "Add"}
+            </div>
           </button>
-          <button className="pay">
-            <img className="dollar-sign-icon" alt="" src="/dollarsign.svg" />
-            <div className="add1">Pay</div>
+          <button className={`pay ${props.value[1] === 0 || props.value[1] === 3 ? `hidden` : ``}`}>
+            <img className="dollar-sign-icon" alt="" src={Dolar} />
+            <div className="font-size-16">Pay</div>
           </button>
         </div>
       </div>
       <div className="middle-text" id="middle_text">
         <p className="p" id="desk_number">
-          2
+          {props.value[0]}
         </p>
         <div className="status" id="status_label">
           <div className="status1">Status:</div>
           <p className="empty" id="status">
-            Empty
+            {props.value[1] === 0 && 'Empty'}
+            {props.value[1] === 1 && 'Taken'}
+            {props.value[1] === 2 && 'Reserved'}
+            {props.value[1] === 3 && 'Clean Up'}
           </p>
         </div>
-        <p className="pm" id="time">
-          01:34:02 PM
+        <p className={`pm ${props.value[1] === 1 || props.value[1] === 2 ? `visible` : ``}`} id="time">
+          {currentTime.toLocaleTimeString()}
         </p>
       </div>
-      <div className="title" id="title">
+      <div className={`title ${getDeskStatus(props.value[1])}`} id="title">
         <p className="empty">Desk</p>
       </div>
     </div>
--- src/pages/Waiter/Waiter.js
+++ src/pages/Waiter/Waiter.js
@@ -3,6 +3,10 @@ import React from 'react'
 import './Waiter.css'
 import toggleIcon from '../../components/UI/Sidebar/assets/icons/sidebar-toggle.png'
 import Profile from '../../components/UI/LoggedProfile/Profile'
+import Desk from '../../components/UI/Desk/Desk'
+
+
+const DeskList = [[1, 0], [4, 1], [45, 3], [2, 3], [12, 2], [33, 1], [7, 2], [19, 0], [26, 3], [41, 2], [50, 1]]
 
 function Waiter({ onToggleSidebar }) {
   return (
@@ -13,7 +17,12 @@ function Waiter({ onToggleSidebar }) {
         </button>
         <Profile />
       </div>
-      <h1>Text Simple</h1>
+      
+      <div className='container'>
+          {DeskList.map((desk, status)=>(
+            <Desk key={status} value={desk} />
+          ))}
+      </div>
     </div>
 
   );
